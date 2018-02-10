2a53c36f19fda31db82c8b03caa67a66e2c6727e
EimanTan <34124245+Eiman24@users.noreply.github.com>
2018-02-10 18:57:41 +0800

    0210

--- chapter 3/3.1.1.py
+++ chapter 3/3.1.1.py
@@ -2,3 +2,17 @@ print(2+2)
 print(50-5*6)
 print((50-5*6)/4)
 print(8/5)
+#除法的取整“//”取余“%”
+print(17//3,17%3,17/3)
+print(5**2,2**7)
+#power 求幂
+width = 20
+height = 5*9
+print(width*height)
+#variable “_” 储存最近print的语句 in interactive mode
+tax=12.5/100
+price=100.50
+print(price * tax)
+print(price + _)
+#round(,2)保留两个小数
+print(round(price*tax,2))
