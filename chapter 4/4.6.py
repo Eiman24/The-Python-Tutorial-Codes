09fe9260076a236a4c84d0bbeddd49beb3c4449f
EimanTan <34124245+Eiman24@users.noreply.github.com>
2018-03-20 15:30:19 +0800

    1530032018

--- chapter 4/4.6.py
+++ chapter 4/4.6.py
@@ -10,3 +10,25 @@ def fib(n):
 
 fib(2000)
 fib(10000)
+
+
+# Global variables cannot be directly assigned a value within a function 
+# (unless named in a global statement), although they may be referenced.
+
+# The actual parameters (arguments) to a function call are introduced 
+# in the local symbol table of the called function when it is called.
+
+# When a function calls another function, a new local symbol table is created for that call.
+
+
+def fib2(n):  # return Fibonacci series up to n
+    """Return a list containing the Fibonacci series up to n."""
+    result = []
+    a, b = 0, 1
+    while a < n:
+        result.append(a)    # see below
+        a, b = b, a+b
+    return result
+
+
+print(fib2(100))
