72ce789b1b298625af41997c1bcd3b4ea85c814c
EimanTan <34124245+Eiman24@users.noreply.github.com>
2018-05-18 13:58:55 +0800

    05172018

--- Chapter 7/7.1.py
+++ Chapter 7/7.1.py
@@ -26,3 +26,27 @@ for x in range(1, 11):
 
 for x in range(1, 11):
 	print('{0:2d} {1:3d} {2:4d}'.format(x, x*x, x*x*x))
+
+
+print('12'.zfill(5))
+print('-3.14'.zfill(7))
+print('3.1415926'.zfill(5))
+
+print('We are the {} who say "{}!"'.format('knights', 'Ni'))
+
+print('{0} and {1}'.format('spam', 'eggs'))
+print('{1} and {0}'.format('spam', 'eggs'))
+
+print('This {food} is {adjective}.'.format(food='spam', adjective='absolutely horrible'))
+
+print('The story of {0}, {1}, and {other}.'.format('Bill', 'Manfred', other='Georg'))
+
+# '!a' apply ascii(), '!s' apply str() and '!r' apply repr()
+contents = 'eels'
+print('My hovercraft is full of {}.'.format(contents))
+print('My hovercraft is full of {!r}.'.format(contents))
+
+list = [1,2,3,'阿短',True]
+tuples = (1,6,4,'呵呵')
+dict = {1:'阿短', 2:'绿豆', 3:'小可', 4:'阿正'}
+print(dict[1])
