01d4e4b8f852c5fe845b96d800037af345c4533d
EimanTan <34124245+Eiman24@users.noreply.github.com>
2018-10-10 16:00:54 +0800

    10102018

--- Chapter 9/9.3.py
+++ Chapter 9/9.3.py
@@ -35,3 +35,46 @@ del x.counter
 
 y = MyClass()
 print(y.f())
+
+#  instance variables are for data unique to each instance 
+# and class variables are for attributes and methods shared by all instances of the class
+class Dog:
+    
+    kind = 'canine'
+    tricks = []
+
+    def __init__(self, name):
+        self.name = name
+
+    def add_trick(self, trick):
+        self.tricks.append(trick)
+
+d = Dog('Fido')
+e = Dog('Buddy')
+print(d.kind, e.kind)
+print(d.name, e.name)
+
+# the mutable object list 'tricks[]' is unexpectedly shared by all dogs
+d.add_trick('roll over')
+e.add_trick('play dead')
+print(d.tricks)
+print(e.tricks)
+
+# Correct the unexpected sharing above
+class Dog2:
+    
+    kind = 'canine'
+
+    def __init__(self, name):
+        self.name = name
+        self.tricks = []
+
+    def add_trick(self, trick):
+        self.tricks.append(trick)
+
+d = Dog2('Fido')
+e = Dog2('Buddy')
+d.add_trick('roll over')
+e.add_trick('play dead')
+print(d.tricks)
+print(e.tricks)
